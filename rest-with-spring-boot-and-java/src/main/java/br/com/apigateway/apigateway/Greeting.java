fc52e5569981e05b408b39d65f6b6715046990c3
Filipe <filipeandrade000@gmail.com>
2023-01-21 03:41:41 -0300

    Organizando pasta

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/apigateway/apigateway/Greeting.java
@@ -0,0 +1,25 @@
+package br.com.apigateway.apigateway;
+
+public class Greeting {
+
+	private final long id;
+	private final String content;
+	
+	
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+}
---
+++ rest-with-spring-boot-and-java/src/test/java/br/com/apigateway/apigateway/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.apigateway.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
