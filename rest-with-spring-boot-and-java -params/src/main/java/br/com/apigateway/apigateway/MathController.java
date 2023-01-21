f7c706d37e4e338693c1245cf300e5150ffee13a
Filipe <filipeandrade000@gmail.com>
2023-01-21 06:18:38 -0300

    sum completo

--- rest-with-spring-boot-and-java -params/src/main/java/br/com/apigateway/apigateway/MathController.java
+++ rest-with-spring-boot-and-java -params/src/main/java/br/com/apigateway/apigateway/MathController.java
@@ -10,14 +10,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 
-	private static final String template = "Hello, %s!";
-
-	private static final AtomicLong counter = new AtomicLong();
-
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sum(@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo) {
-		return 1D;
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null)
+			return 0D;
+
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number))
+			return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null)
+			return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 
 }
