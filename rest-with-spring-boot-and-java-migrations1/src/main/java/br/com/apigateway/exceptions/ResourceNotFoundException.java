4550e3b001285c68f6fa765a01d7eed116926ddc
Filipe <filipeandrade000@gmail.com>
2023-01-26 17:23:40 -0300

    Criando migrations

---
+++ rest-with-spring-boot-and-java-migrations1/src/main/java/br/com/apigateway/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package br.com.apigateway.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	
+	public ResourceNotFoundException(String ex) {
+		super(ex);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
