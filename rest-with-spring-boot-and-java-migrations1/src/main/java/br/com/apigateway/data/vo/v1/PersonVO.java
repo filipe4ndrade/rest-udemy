a34284352eeba6f0cb43066a52ce74ce9a66027f
Filipe <filipeandrade000@gmail.com>
2023-01-27 03:17:13 -0300

    flyway migrate e PersonVO customizada

--- rest-with-spring-boot-and-java-migrations1/src/main/java/br/com/apigateway/data/vo/v1/PersonVO.java
+++ rest-with-spring-boot-and-java-migrations1/src/main/java/br/com/apigateway/data/vo/v1/PersonVO.java
@@ -3,10 +3,16 @@ package br.com.apigateway.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id","lastName","firstName","addres","gender"})
 public class PersonVO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
 	private String lastName;
 	private String addres;
