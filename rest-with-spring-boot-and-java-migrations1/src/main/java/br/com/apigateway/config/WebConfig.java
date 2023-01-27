8c48e88a54ce93e4eac3846306bf55371c99ef62
Filipe <filipeandrade000@gmail.com>
2023-01-27 08:31:38 -0300

    Content Negotiation HEADER

--- rest-with-spring-boot-and-java-migrations1/src/main/java/br/com/apigateway/config/WebConfig.java
+++ rest-with-spring-boot-and-java-migrations1/src/main/java/br/com/apigateway/config/WebConfig.java
@@ -6,18 +6,31 @@ import org.springframework.web.servlet.config.annotation.ContentNegotiationConfi
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
 
-	//http://localhost:8080/api/person/v1/1?mediaType=json
-	
+	/*
+	 * VIA QUERY PARAM //http://localhost:8080/api/person/v1/1?mediaType=json
+	 * 
+	 * @Override public void
+	 * configureContentNegotiation(ContentNegotiationConfigurer configurer) { //
+	 * TODO Auto-generated method stub
+	 * configurer.favorParameter(true).parameterName("mediaType")
+	 * .ignoreAcceptHeader(true).useRegisteredExtensionsOnly(false)
+	 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+	 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+	 * 
+	 * }
+	 */
+
+	// VIA HEADER PARAM http://localhost:8080/api/person/v1/1
+	// Em headers accept escolhe o parâmetro para o postman
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		// TODO Auto-generated method stub
-		configurer.favorParameter(true).parameterName("mediaType")
-		.ignoreAcceptHeader(true).useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
+		configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML);
+
 	}
 
 }
