e98241d2ef0056a36639ae328bdc8d4b6cf0255e
Filipe <filipeandrade000@gmail.com>
2023-02-01 04:31:58 -0300

    user repository e service

---
+++ rest-with-spring-boot-and-java-migrations1/src/main/java/br/com/apigateway/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.apigateway.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.apigateway.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+	@Query("SELECT u FROM User WHERE u.userName =:userName")
+	User findByUsername(@Param("username") String userName);
+}
